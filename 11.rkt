22cb78057233df999e009e5e5db3d25dd82269fe
somber-robot <somberrobot@gmail.com>
2025-08-02 09:52:58 +0100

    Eleventh exercise

---
+++ 11.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(define (encode-modified lst)
+    (define (++ n) (+ n 1))
+    (define (loop clst val count final)
+        (if (empty? clst)
+            (if (zero? count)
+                (append final (list val))
+                (append final (list (list (++ count) val))))
+            (let ((next (car clst))
+                  (tail (cdr clst)))
+                (if (eq? next val)
+                    (loop tail val (++ count) final)
+                    (if (zero? count)
+                        (loop tail next 0 (append final (list val)))
+                        (loop tail next 0 (append final (list (list (++ count) val)))))))))
+    (loop (cdr lst) (car lst) 0 (list)))
+
+(module+ main
+    (encode-modified '(a a a a b c c a a d e e e e)))
