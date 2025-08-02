b24fe1ebb1c739fc24586cf3a0ed57acbcc36db6
somber-robot <somberrobot@gmail.com>
2025-08-02 14:49:09 +0100

    17th exercise

---
+++ 17.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(define (split lst idx)
+    (define (++ n) (+ n 1))
+    (define (loop clst i head tail)
+        (if (empty? clst)
+            (list head tail)
+            (let ((next (car clst))
+                  (rem  (cdr clst)))
+                (if (<= i idx)
+                    (loop rem (++ i) (append head (list next)) tail)
+                    (loop rem (++ i) head (append tail (list next)))))))
+    (loop lst 1 (list) (list)))
+
+(module+ main
+    (split '(a b c d e f g h i k) 3))
