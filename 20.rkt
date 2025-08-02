3484cb4c91b11a1c95ba4c863b2323c4c66af41c
somber-robot <somberrobot@gmail.com>
2025-08-02 15:43:10 +0100

    20th exercise

---
+++ 20.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (remove-at lst idx)
+    (define (++ n) (+ n 1))
+    (define (loop clst i result)
+        (if (empty? clst)
+            result
+            (if (= i idx)
+                (loop (cdr clst) (++ i) result)
+                (loop (cdr clst) (++ i) (append result (list (car clst)))))))
+    (loop lst 1 (list)))
+
+(module+ main
+    (remove-at '(a b c d) 2))
