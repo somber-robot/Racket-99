7d1ebd94bb333a777cf52f1acae0b9bd033bb316
somber-robot <somberrobot@gmail.com>
2025-08-04 13:52:35 +0100

    26th exercise

--- 26.rkt
+++ 26.rkt
@@ -1,27 +1,31 @@
 #lang racket
 
-; a general combination of example (C 3 (A B C D E F))
-; is equal to 
-;   all 3 combinations starting with A of (A B C D E F)
-; + all 3 combinations starting with B of (B C D E F)
-; + all 3 combinations starting with C of (C D E F)
-; + all 3 combinations starting with D of (D E F)
-; ends when remaining pool is the size of count(3)(terminal case)
+(define (combination count lst)
+    (define (-- n) (- n 1))
+    (define (bundle lst)
+        (define (loop clst final)
+            (if (empty? clst)
+                final
+                (let ((value-lst (list (car clst)))
+                      (tail (cdr clst)))
+                    (loop tail (append final (list value-lst))))))
+        (loop lst (list)))
+    (define (outer clst result)
+        (define (inner head tail all)
+            (if (empty? tail)
+                all
+                (let ((next (car tail))
+                      (rem  (cdr tail)))
+                    (inner head rem (append all (list (cons head next)))))))
+        (if (empty? clst)
+            result
+            (let ((head (car clst))
+                  (rem  (cdr clst))
+                  (tail (combination (-- count) (cdr clst))))
+                (outer rem (append result (inner head tail (list)))))))
+    (if (= count 1)
+        (bundle lst)
+        (outer lst (list))))
 
-; each of those break down into:
-; 3 comb w/ A of (A B C D E F)
-; equals A + 
-; all 2 combinations starting with B of (B C D E F)
-; all 2 combinations starting with C of (C D E F)
-; all 2 combinations starting with D of (D E F)
-; all 2 combinations starting with E of (E F)
-; ends when the remaining pool is the size of the new count(2)
-
-; each of those break down into:
-; 2 comb w/ B of (B C D E F)
-; equals B +
-; 1 combination starting with C = C
-; 1 combination starting with D = D
-; 1 combination starting with E = E
-; 1 combination starting with F = F
-; one combinations simply equal the first element of the list
+(module+ main
+    (combination 3 '(a b c d e f)))
