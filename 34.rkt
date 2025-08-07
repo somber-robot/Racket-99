dfd2260de3142264cab2a70eacf3e91e93e9e21e
somber-robot <somberrobot@gmail.com>
2025-08-07 10:33:34 +0100

    34th exercise

---
+++ 34.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (totient-phi m)
+    (define (++ n) (+ n 1))
+    (define (loop i count)
+        (if (= i m)
+            count
+            (if (= (gcd m i) 1)
+                (loop (++ i) (++ count))
+                (loop (++ i) count))))
+    (if (= m 1)
+        m
+        (loop 1 0)))
+
+(module+ main
+    (totient-phi 10)
+    (totient-phi 11)
+    (totient-phi 7))
